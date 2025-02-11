41a1cb69003a2e6b3f3ef687b3a0adb461ee35eb
GooseMagnet <ilankarasik@gmail.com>
2025-02-10 21:53:29 -0500

    Format nvim-cmp

--- pkgs/nvim/lua/cmp.lua
+++ pkgs/nvim/lua/cmp.lua
@@ -1,78 +1,79 @@
 local icons = {
-  Text = ' ',
-  Method = ' ',
-  Function = ' ',
-  Constructor = ' ',
-  Field = ' ',
-  Variable = ' ',
-  Class = ' ',
-  Interface = ' ',
-  Module = ' ',
-  Property = ' ',
-  Unit = ' ',
-  Value = ' ',
-  Enum = ' ',
-  Keyword = ' ',
-  Snippet = ' ',
-  Color = ' ',
-  File = ' ',
-  Reference = ' ',
-  Folder = ' ',
-  EnumMember = ' ',
-  Constant = ' ',
-  Struct = ' ',
-  Event = ' ',
-  Operator = ' ',
-  TypeParameter = ' ',
-  Arrow = ' '
+	Text = " ",
+	Method = " ",
+	Function = " ",
+	Constructor = " ",
+	Field = " ",
+	Variable = " ",
+	Class = " ",
+	Interface = " ",
+	Module = " ",
+	Property = " ",
+	Unit = " ",
+	Value = " ",
+	Enum = " ",
+	Keyword = " ",
+	Snippet = " ",
+	Color = " ",
+	File = " ",
+	Reference = " ",
+	Folder = " ",
+	EnumMember = " ",
+	Constant = " ",
+	Struct = " ",
+	Event = " ",
+	Operator = " ",
+	TypeParameter = " ",
+	Arrow = " ",
 }
 
-local cmp = require('cmp')
+local cmp = require("cmp")
 cmp.setup({
-  snippet = {
-    expand = function(args)
-      vim.snippet.expand(args.body)
-      vim.fn["vsnip#anonymous"](args.body)
-    end,
-  },
-  window = {
-    completion = cmp.config.window.bordered(),
-    documentation = cmp.config.window.bordered(),
-  },
-  mapping = cmp.mapping.preset.insert({
-    ['<c-j>'] = cmp.mapping.select_next_item(),
-    ['<c-k>'] = cmp.mapping.select_prev_item(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-    ['<C-Space>'] = cmp.mapping.complete(),
-  }),
-  sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
-    { name = 'vsnip' },
-    { name = 'buffer' },
-  }),
-  completion = {
-    completeopt = 'menu,menuone,noinsert'
-  },
-  formatting = {
-    fields = { "kind", "abbr", "menu" },
-    format = function(entry, vim_item)
-      local kind = vim_item.kind -- Class, Method, Variable, Text, ...
-      vim_item.kind = icons[kind] or ""
+	snippet = {
+		expand = function(args)
+			vim.snippet.expand(args.body)
+			vim.fn["vsnip#anonymous"](args.body)
+		end,
+	},
+	window = {
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
+	},
+	mapping = cmp.mapping.preset.insert({
+		["<c-j>"] = cmp.mapping.select_next_item(),
+		["<c-k>"] = cmp.mapping.select_prev_item(),
+		["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+		["<C-Space>"] = cmp.mapping.complete(),
+	}),
+	sources = cmp.config.sources({
+		{ name = "nvim_lsp" },
+		{ name = "vsnip" },
+	}, {
+		{ name = "buffer" },
+	}),
+	completion = {
+		completeopt = "menu,menuone,noinsert",
+	},
+	formatting = {
+		fields = { "kind", "abbr", "menu" },
+		format = function(entry, vim_item)
+			local kind = vim_item.kind -- Class, Method, Variable, Text, ...
+			vim_item.kind = icons[kind] or ""
 
-      local max_length = 32 -- Same as IntelliJ
+			local max_length = 32 -- Same as IntelliJ
 
-      if vim_item.abbr:len() > max_length then
-        vim_item.abbr = vim_item.abbr.sub(vim_item.abbr, 1, max_length - 2) .. ".." -- Limit the length
-      end
+			if vim_item.abbr:len() > max_length then
+				vim_item.abbr = vim_item.abbr.sub(vim_item.abbr, 1, max_length - 2) .. ".." -- Limit the length
+			end
 
-      local item = entry:get_completion_item()
-      if item.detail then
-        vim_item.menu = item.detail.sub(item.detail, 1, 23)
-      end
+			local item = entry:get_completion_item()
+			if item.detail then
+				vim_item.menu = item.detail.sub(item.detail, 1, 23)
+			end
 
-      return vim_item
-    end,
-  },
+			return vim_item
+		end,
+	},
 })
 
 local cmp_autopairs = require("nvim-autopairs.completion.cmp")
