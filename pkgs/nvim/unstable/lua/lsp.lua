37f71aef2d3ca5904409f148a54f2c62b68ea98b
GooseMagnet <ilankarasik@gmail.com>
2025-03-29 16:26:44 -0400

    Add keybindings for diagnostics

--- pkgs/nvim/unstable/lua/lsp.lua
+++ pkgs/nvim/unstable/lua/lsp.lua
@@ -1,44 +1,46 @@
 local vim = vim
 
-local lspconfig = require('lspconfig')
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
+local lspconfig = require("lspconfig")
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-lspconfig['gopls'].setup({
-  capabilities = capabilities,
+lspconfig["gopls"].setup({
+	capabilities = capabilities,
 })
-lspconfig['lua_ls'].setup({
-  capabilities = capabilities,
+lspconfig["lua_ls"].setup({
+	capabilities = capabilities,
 })
-lspconfig['nil_ls'].setup({
-  capabilities = capabilities,
+lspconfig["nil_ls"].setup({
+	capabilities = capabilities,
 })
-lspconfig['ts_ls'].setup({
-  capabilities = capabilities,
+lspconfig["ts_ls"].setup({
+	capabilities = capabilities,
 })
 
 vim.api.nvim_create_autocmd("LspAttach", {
-  callback = function(ev)
-    local opts = { buffer = ev.buf }
-    vim.keymap.set("n", "gR", ":Telescope lsp_references<CR>", { desc = "Find References" })
-    vim.keymap.set("n", "gD", vim.lsp.buf.declaration)
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-  end,
+	callback = function(ev)
+		local opts = { buffer = ev.buf }
+		vim.keymap.set("n", "gR", ":Telescope lsp_references<CR>", { desc = "Find References" })
+		vim.keymap.set("n", "gD", vim.lsp.buf.declaration)
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+		vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+		vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+		vim.keymap.set("n", "<leader>D", ":Telescope diagnostics bufnr=0<CR>", { desc = "Show All Diagnostics" })
+		vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, { desc = "Show Diagnostic" })
+	end,
 })
 
 vim.diagnostic.config({
-  virtual_text = {
-    prefix = '●',
-    source = 'always',
-  },
-  signs = true,
-  update_in_insert = false,
-  severity_sort = true,
+	virtual_text = {
+		prefix = "●",
+		source = "always",
+	},
+	signs = true,
+	update_in_insert = false,
+	severity_sort = true,
 })
 
-vim.fn.sign_define('DiagnosticSignError', { text = '', texthl = 'DiagnosticError', linehl = '', numhl = '' })
-vim.fn.sign_define('DiagnosticSignWarn', { text = '', texthl = 'DiagnosticWarn', linehl = '', numhl = '' })
-vim.fn.sign_define('DiagnosticSignInfo', { text = '', texthl = 'DiagnosticInfo', linehl = '', numhl = '' })
-vim.fn.sign_define('DiagnosticSignHint', { text = '', texthl = 'DiagnosticHint', linehl = '', numhl = '' })
+vim.fn.sign_define("DiagnosticSignError", { text = "", texthl = "DiagnosticError", linehl = "", numhl = "" })
+vim.fn.sign_define("DiagnosticSignWarn", { text = "", texthl = "DiagnosticWarn", linehl = "", numhl = "" })
+vim.fn.sign_define("DiagnosticSignInfo", { text = "", texthl = "DiagnosticInfo", linehl = "", numhl = "" })
+vim.fn.sign_define("DiagnosticSignHint", { text = "", texthl = "DiagnosticHint", linehl = "", numhl = "" })
