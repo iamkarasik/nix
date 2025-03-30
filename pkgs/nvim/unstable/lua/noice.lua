8638dcb89ba5a7b151b54072fa41d3cc750a1fb0
GooseMagnet <ilankarasik@gmail.com>
2025-03-30 17:17:25 -0400

    Reduce message noise from noice

--- pkgs/nvim/unstable/lua/noice.lua
+++ pkgs/nvim/unstable/lua/noice.lua
@@ -3,7 +3,31 @@ require("noice").setup({
 		{
 			view = "notify",
 			filter = { event = "msg_showmode" },
-		}
+		},
+		{
+			filter = {
+				event = "msg_show",
+				kind = "",
+				find = "written",
+			},
+			opts = { skip = true },
+		},
+		{
+			filter = {
+				event = "msg_show",
+				kind = "",
+				find = "more lines",
+			},
+			opts = { skip = true },
+		},
+		{
+			filter = {
+				event = "msg_show",
+				kind = "",
+				find = "fewer lines",
+			},
+			opts = { skip = true },
+		},
 	},
 	lsp = {
 		override = {
