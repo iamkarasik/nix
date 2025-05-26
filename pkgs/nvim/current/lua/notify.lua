bbba8f643d78ec5b0a1a783b84e2e13998ef2bc8
GooseMagnet <ilankarasik@gmail.com>
2025-05-25 21:30:37 -0400

    Fix NotifyBackground warning

--- pkgs/nvim/current/lua/notify.lua
+++ pkgs/nvim/current/lua/notify.lua
@@ -2,6 +2,7 @@ local notify = require("notify")
 
 notify.setup({
 	timeout = 1000,
+	background_colour = "#000000"
 })
 
 vim.keymap.set("n", "<C-n>", function() notify.dismiss({ silent = true, pending = true }) end, { desc = "Dismiss notification" })
