c4a0f4328362deb2afaa444509e34ec2de807456
GooseMagnet <ilankarasik@gmail.com>
2025-06-15 13:16:11 -0400

    Add which-key and descriptions for some commands

--- pkgs/nvim/current/lua/toggleterm.lua
+++ pkgs/nvim/current/lua/toggleterm.lua
@@ -1,7 +1,7 @@
 require("toggleterm").setup({
-  open_mapping = [[<c-\>]],
-  direction = 'float', -- 'vertical' | 'horizontal' | 'tab' | 'float'
+	open_mapping = [[<c-\>]],
+	direction = "float", -- 'vertical' | 'horizontal' | 'tab' | 'float'
 })
 
-vim.keymap.set("n", "<leader>tf", ":ToggleTerm direction=float<CR>")
-vim.keymap.set("n", "<leader>th", ":ToggleTerm direction=horizontal<CR>")
+vim.keymap.set("n", "<leader>tf", ":ToggleTerm direction=float<CR>", { desc = "Terminal: Toggle Floating" })
+vim.keymap.set("n", "<leader>th", ":ToggleTerm direction=horizontal<CR>", { desc = "Terminal: Toggle Horizontal" })
