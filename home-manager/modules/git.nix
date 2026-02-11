4723d8768ca1ff9dfb3e28b542cb9a6eba30b3c5
GooseMagnet <ilankarasik@gmail.com>
2026-02-11 17:14:05 -0500

    feat(git): add alias to get url

--- home-manager/modules/git.nix
+++ home-manager/modules/git.nix
@@ -1,10 +1,11 @@
-{pkgs, ...}: {
+{...}: {
   programs.git = {
     enable = true;
 
     settings = {
       alias = {
         oops = "commit --amend --no-edit";
+        url = "config get remote.origin.url";
       };
     };
   };
