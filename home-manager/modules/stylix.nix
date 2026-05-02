5b0ac1fc386e1f713176e32b0f3f5b9b2cb4a04c
iamkarasik <ilankarasik@gmail.com>
2026-05-02 15:46:32 -0400

    fix: stylix error for zen-browser

--- home-manager/modules/stylix.nix
+++ home-manager/modules/stylix.nix
@@ -1,4 +1,5 @@
 {...}: {
   stylix.targets.neovim.enable = false;
   stylix.targets.firefox.enable = false;
+  stylix.targets.zen-browser.profileNames = ["default"];
 }
--- hosts/NixOS/home.nix
+++ hosts/NixOS/home.nix
@@ -13,20 +13,6 @@
     ../../home-manager/modules/zen-browser.nix
   ];
 
-  # gtk = {
-  #   enable = true;
-  #
-  #   theme.package = pkgs.whitesur-gtk-theme;
-  #   theme.name = "WhiteSur-Dark";
-  #
-  #   iconTheme.package = pkgs.whitesur-icon-theme;
-  #   iconTheme.name = "WhiteSur-dark";
-  #
-  #   cursorTheme.package = pkgs.capitaine-cursors;
-  #   cursorTheme.name = "capitaine-cursors-white";
-  #   cursorTheme.size = 30;
-  # };
-
   home.username = username;
   home.homeDirectory = "/home/${username}";
 
