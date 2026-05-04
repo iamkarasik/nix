9ec1549124c1e9eccebab579373559d4b7c1f8f7
iamkarasik <ilankarasik@gmail.com>
2026-05-04 16:45:25 -0400

    fix(stylix): disable zen-browser

--- home-manager/modules/stylix.nix
+++ home-manager/modules/stylix.nix
@@ -1,5 +1,6 @@
 {...}: {
   stylix.targets.neovim.enable = false;
   stylix.targets.firefox.enable = false;
+  stylix.targets.zen-browser.enable = false;
   stylix.targets.zen-browser.profileNames = ["default"];
 }
