8367165b823bf6cb6ae4796117a6da3ac59b3e81
Ilan Karasik <ilankarasik@gmail.com>
2026-08-10 13:34:55 -0400

    Fix wallpaper path syntax in flake.nix

--- flake.nix
+++ flake.nix
@@ -31,7 +31,7 @@
       gitUserName = "iamkarasik";
       gitUserEmail = "ilankarasik@gmail.com";
       homeModule = ./hosts/NixOS/home.nix;
-      wallpaper = ./dotfiles/wallpaper/current;
+      wallpaper = "${./dotfiles/wallpaper}/current";
     };
 
     workSettings = {
@@ -41,7 +41,7 @@
       gitUserName = "Ilan Karasik";
       gitUserEmail = "ikarasik@confluent.io";
       homeModule = ./hosts/MacOS/home.nix;
-      wallpaper = ./dotfiles/wallpaper/current;
+      wallpaper = "${./dotfiles/wallpaper}/current";
     };
 
     overlays = [
