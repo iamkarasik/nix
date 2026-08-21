f928fb0a95b2698fadfe591fe56a369338f4df20
iamkarasik <ilankarasik@gmail.com>
2026-08-21 13:46:38 -0400

    fix(activation): make nix darwin build without sudo & install with sudo

--- activate.sh
+++ activate.sh
@@ -1,5 +1,10 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
 if [[ "$(uname)" == "Darwin" ]]; then
-    sudo darwin-rebuild switch --flake .#MacOS
+    sys=$(nix build .#darwinConfigurations.MacOS.system --no-link --print-out-paths)
+    sudo -H nix-env -p /nix/var/nix/profiles/system --set "$sys"
+    sudo "$sys/sw/bin/darwin-rebuild" activate
 else
     sudo nixos-rebuild switch --flake .#NixOS
 fi
