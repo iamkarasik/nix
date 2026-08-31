c68df469a7a8e9652673c617fbd95c7956ba8137
iamkarasik <ilankarasik@gmail.com>
2026-08-31 14:58:09 -0400

    fix(java): provide data directory to jdtls

--- dotfiles/nvim/lua/autoload/ftplugin/java.lua
+++ dotfiles/nvim/lua/autoload/ftplugin/java.lua
@@ -6,14 +6,20 @@ local ms_java_debug_jar = vim.uv.os_homedir() .. "/.config/nvim/ftplugin/java/ja
 
 local jdtls = require("jdtls")
 
+local root_dir = vim.fs.dirname(
+	vim.fs.find({ "build.gradle", "pom.xml", "gradlew", ".git", "mvnw" }, { upward = true })[1]
+)
+local project_name = vim.fn.fnamemodify(root_dir, ":t")
+local workspace_dir = vim.fn.stdpath("cache") .. "/jdtls/workspace/" .. project_name
+
 local config = {
 	cmd = {
 		jdtls_path,
+		"-data",
+		workspace_dir,
 		"--jvm-arg=-javaagent:" .. lombok_jar,
 	},
-	root_dir = vim.fs.dirname(
-		vim.fs.find({ "build.gradle", "pom.xml", "gradlew", ".git", "mvnw" }, { upward = true })[1]
-	),
+	root_dir = root_dir,
 	capabilities = require("blink.cmp").get_lsp_capabilities(),
 	init_options = {
 		bundles = {
