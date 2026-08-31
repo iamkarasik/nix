85940be8832c45943bb85545477239c3db6a71ba
iamkarasik <ilankarasik@gmail.com>
2026-08-31 15:27:23 -0400

    feat(nvim): add fzf keymaps

--- dotfiles/nvim/lua/plugins/fzf.lua
+++ dotfiles/nvim/lua/plugins/fzf.lua
@@ -1,7 +1,15 @@
 local fzf = require("fzf-lua")
 fzf.register_ui_select(function(ui_opts, _)
 	local prompt = ui_opts.prompt and ui_opts.prompt:gsub("[:%s]+$", "") or "Select"
-	return { prompt = prompt .. "> " }
+	return {
+		prompt = "> ",
+		winopts = {
+			title = " " .. prompt .. " ",
+			title_pos = "center",
+			height = 0.35,
+			width = 0.55,
+		},
+	}
 end)
 
 fzf.setup({
@@ -67,3 +75,7 @@ vim.keymap.set("n", "<leader>D", fzf.lsp_document_diagnostics, { desc = "LSP: Sh
 vim.keymap.set("n", "<leader>fc", function()
 	fzf.live_grep({ search = vim.fn.expand("<cword>") })
 end, { desc = "Find: grep (Current Word)" })
+
+vim.keymap.set("n", "<leader>fs", fzf.lsp_document_symbols, { desc = "Find: symbols (file)" })
+vim.keymap.set("n", "<leader>fS", fzf.lsp_live_workspace_symbols, { desc = "Find: symbols (workspace)" })
+vim.keymap.set("n", "<leader>fr", fzf.resume, { desc = "Find: resume last picker" })
